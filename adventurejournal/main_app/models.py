2687cd52ceeb1636e7bc8e1b4358b4297a79bd3c
taylorlottdixon <136010920+taylorlottdixon@users.noreply.github.com>
2023-08-16 19:45:38 -0500

    build out campaign index and detail pages

--- adventurejournal/main_app/models.py
+++ adventurejournal/main_app/models.py
@@ -4,42 +4,92 @@ from datetime import date
 from django.contrib.auth.models import User
 
 
-class Photo(models.Model):
-  url = models.CharField(max_length=200)
+class CampaignPhoto(models.Model):
+    url = models.CharField(max_length=200)
+    campaign = models.ForeignKey
+
+    def __str__(self):
+        return f"Photo for campaign_id: {self.campaign_id} @{self.url}"
+    
+
+class ProfilePhoto(models.Model):
+    url = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for user {self.user_username} @{self.url}"
 
 
 class System(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} System"
 
 
 class Monster(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} Creature"
 
 
 class Item(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.name} Item"
+
 
 class PlayerCharacter(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} character in {self.campaign_name}"
 
 
 class NPC(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} NPC in {self.campaign_name}"
 
 
 class Encounter(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} encounter in {self.campaign_name}"
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=30)
+    content = models.RichTextField()
+
+    def __str__(self):
+        return f"{self.name} note in {self.campaign_name}"
+
+
+class NoteCategory(models.Model):
+    name = models.CharField(max_length=30)
+    notes = models.ForeignKey(Note, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.name} category in {self.campaign_name}"
+
 
 
 class Campaign(models.Model):
     name = models.CharField(max_length=30)
     system = models.ForeignKey(System, on_delete=models.CASCADE, null=True)
     next_game = models.DateTimeField('Next Game', null=True)
-    players = models.ManyToManyField(PlayerCharacter)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    cover = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True)
-    npcs = models.ManyToManyField(NPC)
+    pcs = models.ForeignKey(PlayerCharacter, on_delete=models.CASCADE, null=True)
+    gm = models.ForeignKey(User, on_delete=models.CASCADE)
+    players = models.ManyToManyField(User, null=True)
+    cover = models.ForeignKey(CampaignPhoto, on_delete=models.CASCADE, null=True)
+    npcs = models.ManyToManyField(NPC, null=True)
+    encounters = models.ForeignKey(Encounter, on_delete=models.CASCADE, null=True)
+    categories = models.ForeignKey(NoteCategory, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.name}"
